3c9f4e21fdc42c3e323ed07f654c57468b99e568
Avis <56881702+Mandehwew@users.noreply.github.com>
2024-09-27 21:50:45 +0700

    Delete Alhavis_Project_Akhir_batch_6 directory
    Ganti File

--- Alhavis_Project_Akhir_batch_6/Dashboard/ultils/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "
-    <script>
-        alert('Logout')
-        window.location.href = 'signin.php'
-    </script>";
--- Alhavis_Project_Akhir_batch_6/database/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "db_bpvp");
-
-// if ($koneksi) {
-//     echo "berhasil";
-// } else {
-//     echo "gagal";
-// }
